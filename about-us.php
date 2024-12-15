dfa20b7822d2042f86965b2d6b712534e08d1510
kevalSK5620 <104882496+kevalSK5620@users.noreply.github.com>
2024-12-15 17:04:09 +0530

    all

---
+++ about-us.php
@@ -0,0 +1,52 @@
+<?php include('shared/_header.php');?>
+
+<body>
+    <main>
+        <div class="big-wrapper light">
+            <img src="./images/shape.png" alt="" class="shape" />
+
+            
+     <?php include('shared/_navbar.php'); ?>
+
+
+
+            <div class="container mt-5">
+
+
+    
+                <div class="card border-0 shadow about-card">
+                    <div class="card-body mt-5 mb-2 mx-3 ">
+                        <h3 class="card-title">
+                        <span class="text-primary icon-hover"><i class="fa-solid fa-paperclip"></i></span>    
+                        About us</h3>
+                        <p class="card-text fs-5 text-justify">
+                        Ganpat University and the township of Ganpat Vidyanagar, a high-tech education campus is a joint initiative, purely philanthropic in nature, by a large number of industrialists and technocrats, noble farmers and affluent businessmen for the mission of “Social Upliftment through Education”. The University is established by the State Government by the enactment of Act No.19/2005 on 12th April 2005. It is approved under section 2(f) by the University Grants Commission (UGC). In consideration of its contribution to the Education in a short period of time, the University has been given Permanent Membership in renowned Academic & Industry Associations, such as the Association of Indian Universities (AIU), Association of Commonwealth Universities (ACU), Federation of Indian Chambers of Commerce and Industry(FICCI), All India Management Association (AIMA), Confederation of Indian Industry(CII), International Association of Universities (IAU), Associated Chambers of Commerce and Industry of India (ASSOCHAM), Association of the Universities of Asia and the Pacific (AUAP), PHD Chamber of Commerce and Industry (PHDCCI), Recognized by Department of Scientific and Industrial Research (DSIR), Govt. of Gujarat appointed Start-up Nodal Institute, Centre for Entrepreneurship Development (CED), Student Start-up & Innovation Policy (SSIP), National Centre for Management of Agricultural Extension (MANAGE).
+
+<br><br>Ganpat University offers various unique, quality, industry-linked and sector-focused Diploma, Undergraduate, Postgraduate and Research level programs (Professional and Non-professional) in the field of Engineering, Management, Computer Applications, Pharmacy, Sciences, Commerce & Social Science, Architecture, Design & Planning, Maritime Studies, Nursing, Agriculture, Physiotherapy & Optometry etc.   
+                    </div>
+                    <div class="mb-4" style="width: 100%;  display: flex; align-items: center; justify-content: center;">
+                    <img style="width: 400px;"  src="images\logo.jpg" class="card-img-bottom" alt="...">
+                    </div>
+                </div>
+
+
+                
+            </div>
+
+
+
+
+
+
+
+
+        </div>
+
+
+    </main>
+
+
+
+
+  
+    <?php include('shared/_footer.php'); ?>
